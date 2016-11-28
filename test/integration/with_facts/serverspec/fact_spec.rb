fb29670a124c6ddff6bec294bc4be15e4728e8cf
Craig R Webster <craig@barkingiguana.com>
2016-11-28 16:22:59 +0000

    Test (and fix) the default case: no facts to add

--- test/integration/with_facts/serverspec/fact_spec.rb
+++ test/integration/with_facts/serverspec/fact_spec.rb
@@ -7,3 +7,11 @@ describe file('/etc/ansible/facts.d') do
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
 end
+
+describe file('/etc/ansible/facts.d/foo.fact') do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode '755' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
